79d0225f825ab1836ae84550c0da3ac2d1efed65
Advaith Rai <advaith_rai@yahoo.com>
2021-02-11 12:44:30 -0600

    sprite added

--- actionstate.js
+++ actionstate.js
@@ -1,17 +1,35 @@
-var hiya = {};
+var hiya = {}, centerX = 1500 / 2, centerY = 1000 / 2, bruce, speed = 4;
 hiya.actionstate = function() {};
 hiya.actionstate.prototype = {
     
     preload: function(){
+        game.load.image('bruce', 'assets/sprites/Dragon_1.png')
         
     },
     create: function() {
         console.log('actionstate');
+        game.stage.backgroundColor = '#6D6214';
         game.input.keyboard.addKey(Phaser.Keyboard.ONE).onDown.add(changeState, null, null, 'victory');
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        
+        bruce = game.add.sprite(centerX, centerY, 'bruce');
+        bruce.anchor.setTo(0.5,0.5);
+        
+       
+       
         
     },
     update: function(){
-    
+        if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+            bruce.x += speed;
+        }
+        else if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+            bruce.x -= speed;
+        }
+        else if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+            bruce.y -= 1;
+            
+        }
     }
     
 };
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(600,400, Phaser.AUTO);
+var game = new Phaser.Game(1500,1000, Phaser.AUTO);
 game.state.add('actionstate',hiya.actionstate);
 game.state.add('victorystate', hiya.victorystate);
 game.state.start('actionstate');
