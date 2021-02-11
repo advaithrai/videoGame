42ae3f24c0401dacc5b7b0c98b29ff81e9a3940d
Advaith Rai <advaith_rai@yahoo.com>
2021-02-11 13:37:40 -0600

    character movement

--- actionstate.js
+++ actionstate.js
@@ -1,19 +1,26 @@
-var hiya = {}, centerX = 1500 / 2, centerY = 1000 / 2, bruce, speed = 4;
+var hiya = {}, centerX = 1500 / 2, centerY = 1000 / 2, bruce, speed = 7;
 hiya.actionstate = function() {};
 hiya.actionstate.prototype = {
     
     preload: function(){
-        game.load.image('bruce', 'assets/sprites/Dragon_1.png')
+        game.load.image('bruce', 'assets/sprites/Dragon_1.png');
+        game.load.image('background', 'assets/backgrounds/dojo.png');
         
     },
     create: function() {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         console.log('actionstate');
         game.stage.backgroundColor = '#6D6214';
         game.input.keyboard.addKey(Phaser.Keyboard.ONE).onDown.add(changeState, null, null, 'victory');
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         
+        var background = game.add.sprite(0,0, 'background');
+        
         bruce = game.add.sprite(centerX, centerY, 'bruce');
-        bruce.anchor.setTo(0.5,0.5);
+        bruce.anchor.setTo(0.5,0.1);
+        bruce.scale.setTo(1.25,1.25);
+        game.physics.enable(bruce);
+        bruce.body.collideWorldBounds = true;
         
        
        
@@ -22,9 +29,11 @@ hiya.actionstate.prototype = {
     update: function(){
         if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
             bruce.x += speed;
+            bruce.scale.setTo(1.25,1.25);
         }
         else if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
             bruce.x -= speed;
+            bruce.scale.setTo(-1.25,1.25);
         }
         else if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
             bruce.y -= 1;
