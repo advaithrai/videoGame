04cd9d3c15af1f82df8be0d185ee4e4eee9b98f5
Advaith Rai <advaith_rai@yahoo.com>
2021-03-07 12:58:54 -0600

    Player has attacks now

--- actionstate.js
+++ actionstate.js
@@ -1,11 +1,12 @@
-var hiya = {}, centerX = 1500 / 2, centerY = 1000 / 2, centerX_1 = 1500, centerY_1 = 1000 / 2,  bruce, masterone,  speed = 7, score = 0, h_health = 100, e_health = 100;
+var hiya = {}, centerX = 1500 / 2, centerY = 1000 / 2, centerX_1 = 1500, centerY_1 = 1000 / 2,  bruce, masterone, platform,  speed = 7, score = 0, h_health = 100, e_health = 100, master_left = true;
 hiya.actionstate = function() {};
 hiya.actionstate.prototype = {
     
     preload: function(){
-        game.load.spritesheet('bruce', 'assets/spritesheets/brucewalk.png', 320, 320);
+        game.load.spritesheet('bruce', 'assets/spritesheets/brucefull.png', 320, 320);
         game.load.spritesheet('masterone', 'assets/spritesheets/masterone.png', 320, 320)
         game.load.image('background', 'assets/backgrounds/dojo.png');
+        game.load.image('platform', 'assets/backgrounds/platform.png');
         
     },
     create: function() {
@@ -16,14 +17,22 @@ hiya.actionstate.prototype = {
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         
         var background = game.add.sprite(0,0, 'background');
+        platform = game.add.sprite(0,340, 'platform');
+        
+        game.physics.enable(platform);
         
         //adds bruce into the game
         bruce = game.add.sprite(centerX, centerY, 'bruce');
         bruce.anchor.setTo(0.5,0.1);
         bruce.scale.setTo(1.25,1.25);
         game.physics.enable(bruce);
+        bruce.body.gravity.y = 1000;
         bruce.body.collideWorldBounds = true;
         bruce.animations.add('walk', [0,1,2,3]);
+        bruce.animations.add('kick', [4,5,6,7,8,9,10,11]);
+        bruce.animations.add('punch',[12,13,14,15,16,17,18]);
+        
+        
         
         
         masterone = game.add.sprite(centerX_1, centerY_1, 'masterone');
@@ -31,18 +40,17 @@ hiya.actionstate.prototype = {
         masterone.scale.setTo(1.25,1.25);
         game.physics.enable(masterone);
         masterone.body.collideWorldBounds = true;
-        masterone.animations.add('walk', [0,1,2,3]);
+        masterone.animations.add('walk', [0,1,2,3,4,5,6,7,8,9,10,11,12]);
+        masterone.animations.add('block', [13,14,15,16,17]);
         
         
         
-        
+        platform.body.immovable = true;
         
         //game.add.text(1300,80, 'Steps: ' + score, {fontSize: 500, fill:'#DA420A'});
         game.add.text(100,10, 'Your Health: ' + h_health, {fontSize: 500, fill:'#DA420A'});
         game.add.text(1200,10, 'Master one Health: ' + e_health, {fontSize: 500, fill:'#DA420A'});
  
-
-        
 
        
        
@@ -50,6 +58,9 @@ hiya.actionstate.prototype = {
     },
     update: function(){
         
+        game.physics.arcade.collide(bruce,[platform, masterone]);
+        
+    
         
         if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
             bruce.x += speed;
@@ -66,19 +77,33 @@ hiya.actionstate.prototype = {
             bruce.scale.setTo(-1.25,1.25);
             bruce.animations.play('walk', 14, true);
         }
+        
         else if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-            bruce.y -= speed;
+            bruce.y -= speed + 20;
             score += 1;
-            if (bruce.y < 360) {
-                bruce.y = 360;
+           if (bruce.y < 360) {
+               bruce.y = 360;
                 bruce.animations.play('walk', 14,true);
-            }
+           }
             
             
         }
+        
+        
+        else if (game.input.keyboard.isDown(Phaser.Keyboard.A)) {
+            bruce.animations.play('kick', 14, false);        }
+        else if (game.input.keyboard.isDown(Phaser.Keyboard.S)) {
+            bruce.animations.play('punch', 14, false);
+        }
+        
+        
         else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
             bruce.y += speed;
             score += 1;
+            if (bruce.y > 450) {
+                bruce.y = 450;
+                bruce.animations.play('walk', 14, true);
+            }
             bruce.animations.play('walk', 14, true);
             
         }
@@ -86,10 +111,39 @@ hiya.actionstate.prototype = {
             bruce.animations.stop('walk');
             bruce.frame = 0;
         }
+
         
-        //Code what masterone has to do 
+        
+        //Code what masterone has to do       
+        
+        if (masterone.x < 500) {
+            master_left = false;
+        }
+        else if (masterone.x >= 1200) {
+            master_left = true;
+        }
+
+        
+        if (master_left == true) {
+            masterone.animations.play('walk', 14, false);
+            masterone.x -= speed;
+            //masterone.animations.stop('walk');
+            masterone.frame = 0;
+            masterone.animations.play('block', 14, false);
+        }
+        else if (master_left == false) {
+            masterone.animations.play('walk', 14, false);
+            masterone.x += speed;
+            masterone.frame = 0;
+           
+        } 
+        if (masterone.x == 600) {
+            masterone.animations.play('block', 30, false);
+            masterone.frame = 0;
+        }
         
         
+       
         
         
     }
